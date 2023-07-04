b1b3afd4f28af07c63eef61d7049e6cb1b0cd555
shivaprasad-rasula <shivaprasadrasula@gmail.com>
2023-07-04 17:32:35 +0530

    added commit to loacl repo

---
+++ src/main/java/com/git/test/Git_Demo/App.java
@@ -0,0 +1,13 @@
+package com.git.test.Git_Demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
